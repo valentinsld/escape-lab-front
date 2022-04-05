d719b128fd23cdd20c1abbb79771c4318d3e0690
valentinsld <valentin.salaud@gmail.com>
2022-04-05 16:06:01 +0200

    feat(main): init socket io

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
+import SocketIO from 'socket.io-client'
 import Vue from 'vue'
+import VueSocketIO from 'vue-socket.io'
 
 import router from '@/router/routes'
 
 import App from './App.vue'
 import store from './store'
 
+const optionsSocket = { transports: ['websocket'] }
+
 Vue.config.productionTip = false
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: SocketIO('https://escape-lab-back.herokuapp.com/', optionsSocket)
+    // vuex: {
+    //   store,
+    //   actionPrefix: 'SOCKET_',
+    //   mutationPrefix: 'SOCKET_'
+    // }
+  })
+)
 
 new Vue({
   store,
