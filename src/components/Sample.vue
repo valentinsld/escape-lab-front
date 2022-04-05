e68178b498101387e160b621c6d554ebb5b0d52f
TimBlanchard <timothee.blanchard37@gmail.com>
2022-04-05 15:17:17 +0200

    feat(vuex): setup vuex store and example in sample component

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="sample">
+    <!-- Routing example -->
     <router-link v-slot="{ href, navigate }" to="/" custom>
       <Link :href="href" :on-click="navigate">Go to connection</Link>
     </router-link>
@@ -7,11 +8,15 @@
     <router-link v-slot="{ href, navigate }" to="game" custom>
       <Link :href="href" :on-click="navigate">Go to game</Link>
     </router-link>
+    <!-- Store mutation example -->
+    <button @click="storeMutation()">action</button>
   </div>
 </template>
 
 <script>
 import Link from '@/components/primitives/Link'
+import { STATE as S } from '@/store/helpers'
+import { MUTATIONS as M } from '@/store/helpers'
 export default {
   name: 'Sample',
   components: {
@@ -22,14 +27,18 @@ export default {
       type: String,
       default: ''
     }
+  },
+  data() {
+    return {
+      stateFromStore: this.$store.state[S.myState]
+    }
+  },
+  methods: {
+    storeMutation() {
+      this.$store.commit(M.myState, 100)
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.sample {
-  h1 {
-    color: red;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 import router from '@/router/routes'
 
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: (h) => h(App)
 }).$mount('#app')
