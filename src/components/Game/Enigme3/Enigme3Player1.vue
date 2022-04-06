6745a6592229ac4717f30c9d5c2e3b1347075078
valentinsld <valentin.salaud@gmail.com>
2022-04-06 10:29:31 +0200

    feat(Enigme3): change name

--- src/components/Game/Enigme3/Enigme3Player1.vue
+++ src/components/Game/Enigme3/Enigme3Player1.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Enigme 1 Player1</h1>
+    <h1>Enigme 3 Player1</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Enigme1Player1'
+  name: 'Enigme3Player1'
 }
 </script>
--- src/components/Game/Enigme3/Enigme3Player2.vue
+++ src/components/Game/Enigme3/Enigme3Player2.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Enigme 1 Player2</h1>
+    <h1>Enigme 3 Player2</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Enigme1Player2'
+  name: 'Enigme3Player2'
 }
 </script>
