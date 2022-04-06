f8255c1920af2ab63474a2443d4ed92ed5352c0e
valentinsld <valentin.salaud@gmail.com>
2022-04-06 10:28:16 +0200

    feat(ViewGame): add tweakpane + subscribe to setStepGame

--- src/views/ViewGame.vue
+++ src/views/ViewGame.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import { Pane } from 'tweakpane'
 import { mapState } from 'vuex'
 
 // enigme 1
@@ -30,6 +31,8 @@ import OutroPlayer from '@/components/Game/Outro/OutroPlayer.vue'
 import { MUTATIONS as M } from '@/store/helpers'
 import { STATE as S } from '@/store/helpers'
 
+const DEBUG = true
+
 export default {
   name: 'ViewGame',
   components: {
@@ -49,6 +52,11 @@ export default {
     OutroPlayer1: OutroPlayer,
     OutroPlayer2: OutroPlayer
   },
+  data() {
+    return {
+      pane: null
+    }
+  },
   computed: mapState([S.typeScreen, S.stepGame]),
   beforeMount() {
     console.log(this.$store.state[S.idRoom])
@@ -57,6 +65,58 @@ export default {
     }
 
     this.$store.commit(M.isStart, true)
+  },
+  mounted() {
+    this.initPane()
+
+    this.sockets.subscribe('setStepGame', ({ stepGame }) => {
+      this.$store.commit(M.stepGame, stepGame)
+    })
+  },
+  unmounted() {
+    // TODO : fix it, not working
+    console.log('unmounted GAME')
+    this.$data.pane.remove()
+  },
+  methods: {
+    initPane() {
+      if (!DEBUG) return
+
+      const pane = new Pane()
+      this.$data.pane = pane
+
+      pane
+        .addButton({
+          title: 'enigme 1'
+        })
+        .on('click', () => {
+          this.$socket.emit('setStepGame', { stepGame: 'Enigme1' })
+        })
+
+      pane
+        .addButton({
+          title: 'enigme 2'
+        })
+        .on('click', () => {
+          this.$socket.emit('setStepGame', { stepGame: 'Enigme2' })
+        })
+
+      pane
+        .addButton({
+          title: 'enigme 3'
+        })
+        .on('click', () => {
+          this.$socket.emit('setStepGame', { stepGame: 'Enigme3' })
+        })
+
+      pane
+        .addButton({
+          title: 'outro'
+        })
+        .on('click', () => {
+          this.$socket.emit('setStepGame', { stepGame: 'Outro' })
+        })
+    }
   }
 }
 </script>
