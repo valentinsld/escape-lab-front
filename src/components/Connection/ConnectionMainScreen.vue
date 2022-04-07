f69526ed1132394239a1169c7791d6dcf8e32b16
valentinsld <valentin.salaud@gmail.com>
2022-04-07 15:00:23 +0200

    feat(ConnectionMainScreen): mainScreen can join room

--- src/components/Connection/ConnectionMainScreen.vue
+++ src/components/Connection/ConnectionMainScreen.vue
@@ -2,10 +2,17 @@
   <div>
     <h1>Connexion Main Screen</h1>
 
-    <div v-if="!idRoom">TRUE</div>
+    <div v-if="!idRoom">Pas de connexion</div>
     <p>{{ idRoom }}</p>
     <p v-if="listUsers.player1">Player 1 :{{ listUsers.player1 }}</p>
     <p v-if="listUsers.player2">Player 2 :{{ listUsers.player2 }}</p>
+
+    <button v-if="!seeJoinRoom" @click="seeJoinRoomClick">Rejoindre une room en cours</button>
+    <div v-if="seeJoinRoom">
+      <p>Rejoindre une room</p>
+      <input ref="inputIdRoom" />
+      <button @click="connectToRoom">Connect to room</button>
+    </div>
   </div>
 </template>
 
@@ -18,13 +25,16 @@ import { STATE_SCREEN } from '@/store/helpers'
 
 export default {
   name: 'ConnectionMainScreen',
+  data() {
+    return {
+      seeJoinRoom: false
+    }
+  },
   computed: mapState({
     idRoom: (state) => state[S.idRoom],
     listUsers: (state) => state[S.listUsers]
   }),
   mounted() {
-    console.log('mounted', this.$socket)
-
     this.$socket.emit('connection')
 
     const loginData = {
@@ -40,6 +50,22 @@ export default {
       this.$store.commit(M.stepGame, 'Intro')
       this.$router.push('/game')
     })
+  },
+  methods: {
+    seeJoinRoomClick() {
+      this.$data.seeJoinRoom = true
+    },
+    connectToRoom() {
+      const idRoom = this.$refs.inputIdRoom.value
+      this.$data.seeJoinRoom = false
+
+      const loginData = {
+        isMainScreen: this.$store.state[S.stateScreen] === STATE_SCREEN.mainScreen,
+        isPlayer: this.$store.state[S.stateScreen] === STATE_SCREEN.player,
+        idRoom
+      }
+      this.$socket.emit('connectToRoom', loginData)
+    }
   }
 }
 </script>
