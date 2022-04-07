4bd996306d32309b836f14c45d7c0f36a9ac8616
valentinsld <valentin.salaud@gmail.com>
2022-04-07 09:51:20 +0200

    feat(intro): link messages & video to back-end

--- src/components/Game/Intro/IntroPlayer.vue
+++ src/components/Game/Intro/IntroPlayer.vue
@@ -1,11 +1,44 @@
 <template>
   <div>
     <h1>Intro Player</h1>
+
+    <div v-for="m in messages" :key="`message${m.id}`">
+      {{ m.text }}
+    </div>
+
+    <h2 v-if="startVideo">VIdeo is started</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IntroPlayer'
+  name: 'IntroPlayer',
+  data() {
+    return {
+      messages: [],
+      startVideo: false
+    }
+  },
+  mounted() {
+    this.initSocketsMessage()
+  },
+  methods: {
+    initSocketsMessage() {
+      // launch start
+      this.$socket.emit('intro-recevedMessage')
+
+      // subscribe new message
+      this.sockets.subscribe('intro-message', (props) => {
+        this.$data.messages.push(props)
+        this.$socket.emit('intro-recevedMessage')
+      })
+
+      // startVideo
+      this.sockets.subscribe('intro-startVideo', () => {
+        console.log('intro-startVideo !!!!!!')
+        this.$data.startVideo = true
+      })
+    }
+  }
 }
 </script>
