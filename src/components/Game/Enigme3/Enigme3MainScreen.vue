b177ca6aa818f336a1619f77a1134e3f4ed8e748
TimBlanchard <timothee.blanchard37@gmail.com>
2022-04-11 15:06:37 +0200

    fix(enigme-3): add if restriction on product annonce

--- src/components/Game/Enigme3/Enigme3MainScreen.vue
+++ src/components/Game/Enigme3/Enigme3MainScreen.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="annonce-product">
+  <div v-if="product" class="annonce-product">
     <h2 v-if="product.name" class="annonce-product__title" v-html="product.name" />
     <p v-if="product.img" class="annonce-product__img" v-html="product.img" />
     <p v-if="product.description" class="annonce-product__description" v-html="product.description" />
