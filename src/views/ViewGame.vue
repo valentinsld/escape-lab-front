049bc0d810743ac9f43226753ce09c585ca6f8e0
valentinsld <valentin.salaud@gmail.com>
2022-04-27 21:34:58 +0200

    feat(ViewGame): active pane ONLY for main screen

--- src/views/ViewGame.vue
+++ src/views/ViewGame.vue
@@ -76,7 +76,7 @@ export default {
   },
   methods: {
     initPane() {
-      // if (!DEBUG || this.$store.state[S.stateScreen] !== STATE_SCREEN.mainScreen) return
+      if (/*!DEBUG || */ this.$store.state[S.stateScreen] !== STATE_SCREEN.mainScreen) return
 
       const pane = new Pane()
       this.$data.pane = pane
