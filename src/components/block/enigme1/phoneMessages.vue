d98d725a222fb3837b52c4a05e97ad6b1af14c85
valentinsld <valentin.salaud@gmail.com>
2022-05-03 21:35:46 +0200

    feat(phoneMessages): fonctional component

--- src/components/block/enigme1/phoneMessages.vue
+++ src/components/block/enigme1/phoneMessages.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>
-    <p>Phone message</p>
+  <div class="container">
+    <p>Phone messageeee</p>
+
+    <div class="list">
+      <div
+        v-for="(message, index) in messages"
+        :key="`message_${message.contact}`"
+        class="list__item"
+        @click="() => setCurrentMessage(index)"
+      >
+        <p>{{ message.contact }}</p>
+        <p v-if="message.messages.length">{{ message.messages[message.messages.length - 1].content }}</p>
+      </div>
+    </div>
+
+    <div class="chat" :class="{ '-open': chatIsOpen }">
+      <div class="chat__header">
+        <p @click="closeChat">Back</p>
+        <p>{{ messages[currentMessage].contact }}</p>
+      </div>
+      <div class="chat__content">
+        <Messages :messages="messages[currentMessage].messages" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Messages from '@/components/block/Messages.vue'
+import INIT_MESSAGE from '@/data/enigme1Messages.js'
+
 export default {
-  name: 'PhoneMessage'
+  name: 'PhoneMessage',
+  components: {
+    Messages
+  },
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentMessage: 0,
+      chatIsOpen: false
+    }
+  },
+  computed: {
+    messages() {
+      console.log(this.data, INIT_MESSAGE)
+      return [...this.data, ...INIT_MESSAGE]
+    }
+  },
+  methods: {
+    setCurrentMessage(index) {
+      this.$data.currentMessage = index
+      this.$data.chatIsOpen = true
+    },
+    closeChat() {
+      this.$data.chatIsOpen = false
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'phoneMessages.scss';
+</style>
