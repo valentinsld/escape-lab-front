f03170f453fce0cfb6d1fcf46bad33a117ecbc15
valentinsld <valentin.salaud@gmail.com>
2022-05-04 10:24:05 +0200

    feat(phoneCall): add sounds

--- src/components/block/enigme1/phoneCall.vue
+++ src/components/block/enigme1/phoneCall.vue
@@ -8,10 +8,21 @@
       <button v-for="num in 9" :key="`button_${num}`" @click="() => clickPad(num)">{{ num }}</button>
       <button @click="() => clickPad(0)">0</button>
     </div>
+
+    <button @click="nextSound">next</button>
   </div>
 </template>
 
 <script>
+/* eslint-disable unused-imports/no-unused-vars, no-unused-vars */
+const SOUNDS_ORIGIN = 'soundsEnigme1/'
+const SOUNDS_STEPS = ['0-intro', '0-wrong', '1-0', '1-1', '1-2', '1-3', '1-wrongCode', '2-1', '2-2-end']
+const PARAMS = {
+  timeBeforeLoop: 500
+}
+
+import { Howl } from 'howler'
+
 import convertSecondToTime from '@/helpers/convertSecondToTIme.js'
 
 export default {
@@ -20,11 +31,20 @@ export default {
     return {
       time: 0,
       timeString: '0:00',
-      numbeEntered: []
+      numbeEntered: [],
+      soundStep: 0,
+      sound: null
+    }
+  },
+  computed: {
+    soundSrc() {
+      const audio = SOUNDS_ORIGIN + SOUNDS_STEPS[this.soundStep]
+      return [audio + '.webm', audio + '.mp3']
     }
   },
   mounted() {
     this.updateTime()
+    this.setSound()
   },
   methods: {
     updateTime() {
@@ -37,6 +57,32 @@ export default {
     clickPad(number) {
       this.$data.numbeEntered.push(number)
       this.$socket.emit('enigme1-enteredNumber', number)
+    },
+
+    //
+    // SOUNDS
+    //
+    setSound() {
+      this.$data.sound?.pause()
+      this.$data.sound = new Howl({
+        src: this.soundSrc,
+        autoplay: true,
+        volume: 0.5,
+        onend: () => {
+          console.log('Finished!')
+          setTimeout(() => this.$data.sound.play(), PARAMS.timeBeforeLoop)
+        }
+      })
+
+      this.$data.sound.play()
+    },
+    nextSound(ev, setIndex = null) {
+      console.log(this.$data.soundStep, setIndex, SOUNDS_STEPS.length)
+      this.$data.soundStep = setIndex || this.$data.soundStep + 1
+
+      if (this.$data.soundStep >= SOUNDS_STEPS.length) return
+
+      this.setSound()
     }
   }
 }
