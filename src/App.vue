7d33c15205063ff8452f52c881f5bd40869155ac
TimBlanchard <timothee.blanchard37@gmail.com>
2022-05-04 15:35:42 +0200

    style(app): remove global text-align center

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,6 @@ export default {
   height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
-  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
