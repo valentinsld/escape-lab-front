901d34fa4063b14ffe48f4d7a61ccd724c99fe51
TimBlanchard <timothee.blanchard37@gmail.com>
2022-05-06 09:31:40 +0200

    feat(notice): add next and prev btn + images

--- src/components/Game/Enigme3/Enigme3Player2.vue
+++ src/components/Game/Enigme3/Enigme3Player2.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="notice">
     <div class="notice__rules-container">
-      <div v-for="i in 2" :key="i" class="notice__rule">
-        <img :src="getSource(i)" />
+      <div v-for="i in 2" :key="i" :class="`notice__rule notice__rule--${i}`" :style="ruleStyle(i)">
+        <img class="notice__rule__img" :src="getSource(i)" />
       </div>
+      <button @click="prevQuestion">Prev</button>
+      <button @click="nextQuestion">Next</button>
     </div>
   </div>
 </template>
@@ -14,7 +16,8 @@ export default {
   name: 'Enigme3Player2',
   data() {
     return {
-      rules: noticeData()
+      rules: noticeData(),
+      currentPage: 1
     }
   },
   sockets: {
@@ -28,12 +31,31 @@ export default {
     },
     getSource(i) {
       return require(`@/assets/images/enigme3/notice/page-${i}.png`)
+    },
+    ruleStyle(i) {
+      return {
+        zIndex: 100 - i
+      }
+    },
+    nextQuestion() {
+      if (this.currentPage < 2) this.currentPage += 1
+    },
+    prevQuestion() {
+      if (this.currentPage > 1) this.currentPage -= 1
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+.notice {
+  height: 100vh;
+  background: #3577f5;
+}
+
 .notice__rule {
   position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
