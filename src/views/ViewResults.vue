63406bd209f135de2034516ce41ca49b97b5e738
Robin Verona <robin.verona@edu.gobelins.fr>
2022-05-24 20:53:14 +0200

    feat(enigme2_ui): add style to enigme2 && page result from mockups

---
+++ src/views/ViewResults.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <img src="images/Et en vrai.png" alt="" />
+    <div class="scrolling-wrapper">
+      <div class="card">
+        <img src="images/card1.png" alt="" />
+      </div>
+      <div class="card">
+        <img src="images/card2.png" alt="" />
+      </div>
+      <div class="card">
+        <img src="images/card3.png" alt="" />
+      </div>
+      <div class="card">
+        <img src="images/card4.png" alt="" />
+      </div>
+      <div class="card">
+        <img src="images/card5.png" alt="" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+html,
+body {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  background-color: #4a6cff;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.scrolling-wrapper {
+  display: flex;
+  flex-wrap: nowrap;
+  height: 100%;
+  margin-top: 100px;
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+}
+
+.card {
+  flex: 0 0 auto;
+  margin: 10px;
+}
+</style>
