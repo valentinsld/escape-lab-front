e4759b92d9764581e25855ca6548cd9a27d108b7
valentinsld <valentin.salaud@gmail.com>
2022-05-24 22:35:51 +0200

    feat(outro): outrooooooooooooooooo

--- src/components/Game/Outro/OutroMainScreen.vue
+++ src/components/Game/Outro/OutroMainScreen.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
-    <h1>Outro MainScreen</h1>
+  <div class="outro" :class="endVideo ? '-black' : ''">
+    <Button v-if="endVideo" text="Voir des statistiques" :on-click="goToStats" />
   </div>
 </template>
 
 <script>
+import Button from '@/components/block/button'
+
 export default {
-  name: 'OutroMainScreen'
+  name: 'OutroMainScreen',
+  components: {
+    Button
+  },
+  data() {
+    return {
+      endVideo: false
+    }
+  },
+  sockets: {
+    endVideo: function () {
+      console.log('endVideo')
+      this.endVideo = true
+    }
+  },
+  methods: {
+    goToStats() {
+      this.$router.push({ path: '/results' })
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.outro {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  transition: all 500ms var(--custom-bezier);
+
+  &.-black {
+    background-color: var(--color-black);
+  }
+}
+</style>
