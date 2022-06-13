d2e0c38aa2929082b6f54f4837690727f8c352f1
Robin Verona <robin.anorev@proton.me>
2022-06-13 11:49:25 +0200

    feat(dataviz): add desktop style from mockups as draft
    wip

--- src/views/ViewResults.vue
+++ src/views/ViewResults.vue
@@ -1,59 +1,70 @@
 <template>
-  <swiper id="ResultsCardsSlider" class="swiperTest" :scrollbar="{ draggable: true }" mousewheel free-mode navigation>
-    <!-- <div class="inner-slider"> -->
-    <swiper-slide style="width: 480px">
-      <ResultCard
-        :image="Person"
-        title="Un problème qui touche aussi les jeunes"
-        main-text="Si on a tendance à penser que les personnes qui se font le plus arnaquer sont les séniors, il s’agit en réalité des 15-29 ans. Une arnaque sur deux se déroule en effet sur internet, lieu d’échange de prédilection de cette tranche d’âge, qui s’expose sans le savoir.
+  <div id="dataviz">
+    <h1>Et en vrai ?</h1>
+    <swiper
+      id="ResultsCardsSlider"
+      class="swiperTest"
+      slides-per-view="auto"
+      :scrollbar="{ draggable: true }"
+      mousewheel
+      free-mode
+      navigation
+    >
+      <!-- <div class="inner-slider"> -->
+      <swiper-slide style="width: 480px">
+        <ResultCard
+          :image="Person"
+          title="Un problème qui touche aussi les jeunes"
+          main-text="Si on a tendance à penser que les personnes qui se font le plus arnaquer sont les séniors, il s’agit en réalité des 15-29 ans. Une arnaque sur deux se déroule en effet sur internet, lieu d’échange de prédilection de cette tranche d’âge, qui s’expose sans le savoir.
           En revanche, les montants escroqués aux +70 ans, les seconds plus arnaqués en France, sont supérieurs à ceux escroqués aux jeunes, alors soyez sympa, protégez vos aînés, passez le mot !"
-        :has-tips="false"
-        tips-text="none"
-      />
-    </swiper-slide>
-    <swiper-slide style="width: 480px">
-      <ResultCard
-        :image="Machine"
-        title="le Ping Call"
-        main-text="Une des arnaques les plus classiques, et pouvant aboutir à des conséquences financières lourdes, le Ping Call consiste à faire rappeler un numéro surtaxé en se faisant passer, en général, pour un agent d’une compagnie d’énergie ou de téléphonie."
-        :has-tips="true"
-        tips-text="Orange rappelle à ses utilisateurs que les numéros commençant par 081, 082 et 089 sont tous 
+          :has-tips="false"
+          tips-text="none"
+        />
+      </swiper-slide>
+      <swiper-slide style="width: 480px">
+        <ResultCard
+          :image="Machine"
+          title="le Ping Call"
+          main-text="Une des arnaques les plus classiques, et pouvant aboutir à des conséquences financières lourdes, le Ping Call consiste à faire rappeler un numéro surtaxé en se faisant passer, en général, pour un agent d’une compagnie d’énergie ou de téléphonie."
+          :has-tips="true"
+          tips-text="Orange rappelle à ses utilisateurs que les numéros commençant par 081, 082 et 089 sont tous 
 des numéros surtaxés."
-      />
-    </swiper-slide>
-    <swiper-slide style="width: 480px">
-      <ResultCard
-        :image="Alert"
-        title="Le faux courrier"
-        main-text="Vue et revue dans nos boîtes mails et dan nos DM instagram, l’arnaque aux spams et au courrier frauduleux est toutefois encore toujours présente. 
+        />
+      </swiper-slide>
+      <swiper-slide style="width: 480px">
+        <ResultCard
+          :image="Alert"
+          title="Le faux courrier"
+          main-text="Vue et revue dans nos boîtes mails et dan nos DM instagram, l’arnaque aux spams et au courrier frauduleux est toutefois encore toujours présente. 
 Il est aussi important de se rappeler que les faux courriers existent aussi par voie postale, et toujours vérifier l’expéditeur."
-        :has-tips="true"
-        tips-text="On pense à vérifier les expéditeurs de nos courriers et mails, et on fait attention aux liens pour se connecter."
-      />
-    </swiper-slide>
-    <swiper-slide style="width: 480px">
-      <ResultCard
-        :image="Bag"
-        title="Les fausses ventes de produits ou services"
-        main-text="Parmi les arnaques les plus répandues, on peut citer les fausses ventes de produits ou de services : qui n’a jamais commandé un produit en ligne, pour recevoir un produit totalement non conforme, voir ne jamais le recevoir !"
-        :has-tips="true"
-        tips-text="Prenez garde à toujours vérifier les avis et les notes du vendeur, pour vous assurer de sa véracité 
+          :has-tips="true"
+          tips-text="On pense à vérifier les expéditeurs de nos courriers et mails, et on fait attention aux liens pour se connecter."
+        />
+      </swiper-slide>
+      <swiper-slide style="width: 480px">
+        <ResultCard
+          :image="Bag"
+          title="Les fausses ventes de produits ou services"
+          main-text="Parmi les arnaques les plus répandues, on peut citer les fausses ventes de produits ou de services : qui n’a jamais commandé un produit en ligne, pour recevoir un produit totalement non conforme, voir ne jamais le recevoir !"
+          :has-tips="true"
+          tips-text="Prenez garde à toujours vérifier les avis et les notes du vendeur, pour vous assurer de sa véracité 
 et de sa qualité."
-      />
-    </swiper-slide>
-    <swiper-slide style="width: 480px">
-      <ResultCard
-        :image="IdCard"
-        title="L’usurpation d’identité"
-        main-text="Sous prétexte d’une vente d’un objet, ou d’une visite de logement, votre interlocuteur vous demande de fournir des documents officiels : pièces d’identité, bulletins de salaire, … Attention, certaines personnes malveillantes s’en servent pour ouvrir des crédits à la consommation à votre nom !"
-        :has-tips="true"
-        tips-text="Lors d’une visite d’appartement,
+        />
+      </swiper-slide>
+      <swiper-slide style="width: 480px">
+        <ResultCard
+          :image="IdCard"
+          title="L’usurpation d’identité"
+          main-text="Sous prétexte d’une vente d’un objet, ou d’une visite de logement, votre interlocuteur vous demande de fournir des documents officiels : pièces d’identité, bulletins de salaire, … Attention, certaines personnes malveillantes s’en servent pour ouvrir des crédits à la consommation à votre nom !"
+          :has-tips="true"
+          tips-text="Lors d’une visite d’appartement,
 pensez à ajouter un filigrane
 &laquo;document réservé à la recherche de logement&raquo; sur votre dossier."
-      />
-    </swiper-slide>
-    <!-- </div> -->
-  </swiper>
+        />
+      </swiper-slide>
+      <!-- </div> -->
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -120,9 +131,24 @@ export default {
   display: flex !important;
   gap: 20px 20px;
 } */
-#ResultsCardsSlider {
+
+#dataviz {
+  width: 100%;
+  height: 100%;
   background-color: var(--color-blue);
-  border: greenyellow solid 1px;
+}
+
+#dataviz h1 {
+  font-size: 75px;
+  color: var(--color-white);
+  text-shadow: 4px 1px 0 var(--color-black);
+  -webkit-text-stroke: 1px var(--color-black);
+
+  /* font-weight: bold; */
+}
+
+#ResultsCardsSlider {
+  height: 100%;
 }
 
 .swiper-slide {
@@ -137,29 +163,24 @@ export default {
 
 .swiper-slide:first-of-type {
   margin-left: 300px;
-  border: red solid 3px;
 }
 
 .swiper-slide:last-of-type {
   margin-right: 600px;
-  border: greenyellow solid 3px;
 }
 
 .swiper-container {
   width: 100%;
   height: 450px;
-  border: red solid 2px;
 }
 
 @media screen and (max-width: 767px) {
   .swiper-slide:first-of-type {
     margin-left: 0;
-    border: red solid 3px;
   }
 
   .swiper-slide:last-of-type {
     margin-right: 0;
-    border: greenyellow solid 3px;
   }
 }
 </style>
