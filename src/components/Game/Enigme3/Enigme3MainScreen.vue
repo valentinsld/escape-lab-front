d64bdf93613465bc311c972cffe20e0c35b5eb51
valentinsld <valentin.salaud@gmail.com>
2022-06-16 15:34:20 +0200

    feat(Enigme3MainScreen): new UI

--- src/components/Game/Enigme3/Enigme3MainScreen.vue
+++ src/components/Game/Enigme3/Enigme3MainScreen.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="annonce-product" :style="`--scale: ${scale}`">
     <div v-if="product" class="annonce-product__wrapper">
+      <h2
+        v-if="product.name && product.subtype.val"
+        class="annonce-product__title"
+        v-html="product.name + ' ' + product.subtype.val"
+      />
       <div class="annonce-product__row">
         <div class="annonce-product__left-column">
           <div class="annonce-product__img-container">
@@ -8,11 +13,18 @@
           </div>
         </div>
         <div class="annonce-product__right-column">
+          <div v-if="product.description" class="annonce-product__description">
+            <p v-for="(item, index) in product.description" :key="index" v-html="item" />
+          </div>
+        </div>
+      </div>
+
+      <div class="annonce-product__row">
+        <div class="annonce-product__left-column">
           <div class="annonce-product__sailer">
-            <div class="annonce-product__sailer-name">
-              <img src="@/assets/images/enigme3/sailer-profil.png" />
-              <h4 v-html="` Vendu par <br> <span>${textContent.sailerName}</span> `" />
-            </div>
+            <h4 class="annonce-product__sailer-name">
+              Vendu par <span>{{ textContent.sailerName }}</span>
+            </h4>
             <div v-if="sailer" class="annonce-product__sailer-data">
               <p v-if="sailer.sales && sailer.reviews" v-html="`${sailer.sales} ventes - ${sailer.reviews} avis`" />
               <p v-if="sailer.date" v-html="`Membre depuis ${sailer.date}`" />
@@ -20,20 +32,10 @@
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="annonce-product__row">
-        <div class="annonce-product__left-column">
-          <h2 v-if="product.name" class="annonce-product__title" v-html="product.name" />
-          <h4 v-if="product.subtype.text" v-html="product.subtype.text" />
-          <div v-if="product.description" class="annonce-product__description">
-            <p v-for="(item, index) in product.description" :key="index" v-html="item" />
-          </div>
-        </div>
 
         <div class="annonce-product__right-column">
           <div v-if="product.criteria.good" class="annonce-product__criteria-container">
-            <h3>Caractéristiques techniques</h3>
+            <h3>Détails techniques</h3>
             <div v-for="(criteria, name) in product.criteria.good" :key="name" class="annonce-product__criteria-item">
               <p v-if="criteriaName[name]" class="annonce-product__criteria-name" v-html="criteriaName[name]" />
               <p class="annonce-product__criteria-value" v-html="criteria" />
@@ -103,10 +105,10 @@ export default {
         : normalSailers[randomNum(0, normalSailers.length)]
     },
     initScale() {
-      this.scale = Math.min(window.innerWidth / 1920, window.innerHeight / 1080) * 0.92
+      this.scale = Math.min(window.innerWidth / 1920, window.innerHeight / 1080) * 0.87
 
       window.addEventListener('resize', () => {
-        this.scale = Math.max(window.innerWidth / 1920, window.innerHeight / 1080) * 0.92
+        this.scale = Math.max(window.innerWidth / 1920, window.innerHeight / 1080) * 0.87
       })
     }
   }
@@ -115,8 +117,8 @@ export default {
 <style lang="scss" scoped>
 .annonce-product {
   position: absolute;
-  top: 42.5vh;
-  left: 49.33%;
+  top: 42.8vh;
+  left: 49.1%;
   width: 986px;
   height: 560px;
   transition: opacity 400ms var(--custom-bezier);
@@ -126,6 +128,15 @@ export default {
   p {
     margin: 5px 0;
   }
+}
+
+.annonce-product__title {
+  margin-top: 0;
+  font-size: 2.3em;
+  font-weight: var(--weight-bold);
+  text-align: center;
+  text-shadow: 2px 0 0 var(--color-black);
+  letter-spacing: 0.5px;
 }
 
 .annonce-product__site-title {
@@ -141,19 +152,35 @@ export default {
 }
 
 .annonce-product__wrapper {
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   height: 100%;
-  padding: 2.2em 3em;
+  padding: 2.2em 5em;
   overflow: hidden;
   background: var(--color-white);
   border: 5px solid var(--color-black);
-  border-radius: 170px;
+  border-radius: 160px;
+
+  &::before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    content: '';
+    background-color: #b5a79e;
+    opacity: 0.25;
+  }
 }
 
 .annonce-product__description {
   margin-top: 45px;
+  font-size: 1.15em;
 }
 
 .annonce-product__row {
@@ -166,21 +193,33 @@ export default {
 }
 
 .annonce-product__left-column {
-  width: 53%;
+  width: 47%;
 }
 
 .annonce-product__right-column {
-  width: 40%;
+  width: 45%;
 }
 
 .annonce-product__sailer-name {
-  display: flex;
-  align-items: center;
+  margin-top: 0;
+  margin-bottom: 10px;
+  font-size: 2em;
+  line-height: 1.65;
 
-  h4 {
-    margin-left: 20px;
-    font-size: 1.4em;
-    line-height: 1.65;
+  span {
+    text-decoration: underline;
+  }
+}
+
+.annonce-product__sailer-data {
+  margin-top: 20px;
+
+  p {
+    font-size: 1.3em;
+
+    &:nth-child(1) {
+      font-weight: var(--weight-bold);
+    }
   }
 }
 
@@ -188,13 +227,16 @@ export default {
   // margin-top: 50px;
 
   h3 {
-    margin-bottom: 35px;
-    font-size: 1.3em;
+    margin-top: 0;
+    margin-bottom: 10px;
+    font-size: 2em;
+    line-height: 1.65;
   }
 }
 
 .annonce-product__criteria-item {
   display: flex;
+  font-size: 1.15em;
 }
 
 .annonce-product__criteria-name {
@@ -205,9 +247,5 @@ export default {
 
 .annonce-product__criteria-value {
   margin: 0;
-}
-
-.annonce-product__sailer-data {
-  margin-top: 20px;
 }
 </style>
