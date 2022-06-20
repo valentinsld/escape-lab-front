855c723c1d7b30741712c190c4fdc433d33e422d
valentinsld <valentin.salaud@gmail.com>
2022-06-20 14:12:23 +0200

    feat(ConnectionMainScreen): on high mode wrong QRCode

--- src/components/Connection/ConnectionMainScreen.vue
+++ src/components/Connection/ConnectionMainScreen.vue
@@ -26,7 +26,12 @@
 
         <div class="content__connect">
           <div class="connect__qrcode">
-            <QrcodeVue v-if="idRoom" :value="urlQrCode + idRoom" :size="110" level="H" />
+            <QrcodeVue
+              v-if="idRoom"
+              :value="highmode ? 'https://www.twitch.tv/GobelinsLIVE' : urlQrCode + idRoom"
+              :size="110"
+              level="H"
+            />
           </div>
           <p v-if="idRoom" class="connect__text">
             ou rendez-vous sur<br />
